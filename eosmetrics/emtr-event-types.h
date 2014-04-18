af804d2f17701dc025d3cef99b3804d6e3793b5d
matt arkin <markin@endlessm.com>
2014-04-18 09:15:52 -0700

    Added Network Status Change Type

--- eosmetrics/emtr-event-types.h
+++ eosmetrics/emtr-event-types.h
@@ -36,6 +36,19 @@ G_BEGIN_DECLS
  */
 #define EMTR_EVENT_USER_LOGGED_IN "ab839fd2-a927-456c-8c18-f1136722666b"
 
+/**
+ * EMTR_EVENT_NETWORK_STATUS_CHANGED:
+ *
+ * Recorded when the network changes from one of the states described at
+ * https://developer.gnome.org/NetworkManager/unstable/spec.html#type-NM_STATE
+ * to another. The auxiliary payload is a 2-tuple of the form
+ * (previous_network_state, new_network_state). Since events are delivered on a
+ * best-effort basis, there is no guarantee that the new network state of the
+ * previous successfully recorded network status change event matches the
+ * previous network state of the current network status change event.
+ */
+#define EMTR_EVENT_NETWORK_STATUS_CHANGED "5fae6179-e108-4962-83be-c909259c0584"
+
 G_END_DECLS
 
 #endif /* EMTR_EVENT_TYPES_H */

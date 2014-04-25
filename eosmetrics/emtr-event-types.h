ef3e71f7d5d18dce78b7c6abdcc9df86cd2552a3
Devin Ekins <DevinJ.Ekins@gmail.com>
2014-04-25 16:29:16 -0600

    Added app open/close types.
    These are needed for the shell to register when an application opens or closes.

--- eosmetrics/emtr-event-types.h
+++ eosmetrics/emtr-event-types.h
@@ -49,6 +49,18 @@ G_BEGIN_DECLS
  */
 #define EMTR_EVENT_NETWORK_STATUS_CHANGED "5fae6179-e108-4962-83be-c909259c0584"
 
+/**
+ * EMTR_EVENT_SHELL_APP_IS_OPEN:
+ * 
+ * Occurs when an application visible to the shell is opened or closed. The payload
+ * varies depending on whether it is given as an opening event or a closed event.
+ * If it is an opening event, the payload is a human-readable application name.
+ * If it is a closing event, the payload is empty. The key used is a pointer to the
+ * corresponding ShellApp.
+ */
+
+#define EMTR_EVENT_SHELL_APP_IS_OPEN "b5e11a3d-13f8-4219-84fd-c9ba0bf3d1f0"
+
 G_END_DECLS
 
 #endif /* EMTR_EVENT_TYPES_H */

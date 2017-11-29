ea069ee7a95401761c437e98530913354c7d507b
Will Thompson <wjt@endlessm.com>
2017-11-29 16:22:07 +0000

    tests: build test-daemon with mock cache size provider
    8b1e2ba17c88cf37f9d12d93ed73e46fe5dfeb6a caused the test which just runs
    emer_daemon_new (NULL, NULL) to fail when SYSCONFDIR is not an existing,
    writable directory. This is because, if EmerDaemon:persistent-cache is
    NULL at construct time, it constructs its own, calling
    emer_cache_size_provider_get_max_cache_size (NULL) where NULL indicates
    "use the default path".  emer_cache_size_provider_get_max_cache_size()
    attempts to create the target file if it doesn't already exist with
    sensible contents.
    
    Previouly, the call to emer_cache_size_provider_get_max_cache_size() was
    within emer-persistent-cache.c. test-daemon does not use the real
    EmerPersistentCache, but the mock one in mock-persistent-cache.c which
    did not call emer_cache_size_provider_get_max_cache_size()!
    
    Following the example set by other tests' approach to dependency
    injection, namely "compile a different C file with a different
    implementation of the function", add a new mock-cache-size-provider.c
    for test-daemon's benefit. It asserts that the path passed is NULL since
    it is only intended to be used in the "use-default-path" case.
    
    https://phabricator.endlessm.com/T19953

---
+++ tests/daemon/mock-cache-size-provider.c
@@ -0,0 +1,40 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/* Copyright 2017 Endless Mobile, Inc. */
+
+/*
+ * This file is part of eos-event-recorder-daemon.
+ *
+ * eos-event-recorder-daemon is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * eos-event-recorder-daemon is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with eos-event-recorder-daemon.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "emer-cache-size-provider.h"
+
+/* This is the default maximum cache size in bytes. */
+#define DEFAULT_MAX_CACHE_SIZE G_GUINT64_CONSTANT (10000000)
+
+guint64
+emer_cache_size_provider_get_max_cache_size (const gchar *path)
+{
+  /* This mock should only be used by test-daemon.c when testing emer-daemon's
+   * logic to construct the persistent cache itself (as used in production). In
+   * that case, the daemon should attempt to load the cache size from the
+   * default path, which is expressed as NULL. Fail any test which accidentally
+   * attempts to use this mock to read a real file.
+   */
+  g_assert_cmpstr (path, ==, NULL);
+
+  return DEFAULT_MAX_CACHE_SIZE;
+}

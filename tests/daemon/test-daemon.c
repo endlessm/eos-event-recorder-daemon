be453c738d6ad7d078816ab541bc0ca66b52873e
Will Thompson <wjt@endlessos.org>
2022-06-13 23:36:15 +0100

    test-daemon: Use g_assert_cmpvariant
    5ba24a98452dced23ade5fcca7c233e2bd5ed252 "test-daemon: print variants
    when different" predates the addition of this macro to GLib.

--- tests/daemon/test-daemon.c
+++ tests/daemon/test-daemon.c
@@ -475,22 +475,10 @@ assert_variants_equal (GVariant *actual_variant,
                        GVariant *expected_variant)
 {
   g_assert_nonnull (actual_variant);
+  g_assert_nonnull (expected_variant);
 
-  if (!g_variant_equal (actual_variant, expected_variant))
-    {
-      /* Assertion failures of the form
-       *   ERROR:../tests/daemon/test-daemon.c:495:assert_variants_equal:
-       *   'g_variant_equal (actual_variant, expected_variant)' should be TRUE
-       * are not very helpful -- you want to see the actual variants!
-       */
-      g_autofree gchar *actual_str = g_variant_print (actual_variant, TRUE);
-      g_autofree gchar *expected_str = g_variant_print (expected_variant, TRUE);
-      g_assert_cmpstr (actual_str, ==, expected_str);
+  g_assert_cmpvariant (actual_variant, expected_variant);
 
-      /* Should not be reached: */
-      g_error ("variants compared non-equal, but their type-annotated "
-               "stringified representations compared equal!");
-    }
   g_variant_unref (actual_variant);
   g_variant_unref (expected_variant);
 }
